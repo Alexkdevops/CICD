599907681d508dc83b8b832e726b93d21ed8dfa4
Alexkdevops <alexkad.devops@gmail.com>
2022-01-09 20:10:07 +0000

    Check

--- del.sh
+++ del.sh
@@ -3,3 +3,5 @@ kubectl delete deploy alex-api -n prod
 kubectl delete deploy alex-web -n prod
 kubectl delete deploy alex-api -n dev
 kubectl delete deploy alex-web -n dev
+kubectl delete deploy alex-api -n main
+kubectl delete deploy alex-web -n main
