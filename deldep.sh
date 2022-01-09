fe0f8c35a90e52729aea34f92b2b245ff50290fb
Alexkdevops <alexkad.devops@gmail.com>
2022-01-09 18:53:53 +0000

    Check

--- deldep.sh
+++ deldep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-k delete deploy alex-api -n prod
-k delete deploy alex-web -n prod
-k delete deploy alex-api -n dev
-k delete deploy alex-web -n dev
+kubectl delete deploy alex-api -n prod
+kubectl delete deploy alex-web -n prod
+kubectl delete deploy alex-api -n dev
+kubectl delete deploy alex-web -n dev
