250631d99057eb71b4056fc79cd34aa1138aee59
Alexkdevops <alexkad.devops@gmail.com>
2022-01-09 19:17:13 +0000

    check

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 export AWS_DEFAULT_REGION=us-east-2
-cd api/
-make deploy
-cd ..
-cd web/
-make deploy
-cd ..
+
+if [[ $BRANCH_NAME == "main" ]]
+then
+    cd api/
+    make build namespace=main
+    make push namespace=main
+    make deploy namespace=main
+    cd ..
+    cd web/
+    make build namespace=main
+    make push namespace=main
+    make deploy namespace=main
+    cd ..
+elif [[ $BRANCH_NAME == "dev" ]]
+then
+    cd api/
+    make build namespace=dev
+    make push namespace=dev
+    make deploy namespace=dev
+    cd ..
+    cd web/
+    make build namespace=dev
+    make push namespace=dev
+    make deploy namespace=dev
+    cd ..
+elif [$BRANCH_NAME == "prod"]
