80881c89f9511f7acccd833b2a8a5800db25f127
Alexkdevops <alexkad.devops@gmail.com>
2022-01-09 19:41:29 +0000

    Check

--- deploy.sh
+++ deploy.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 
+# this will force script to exit if any command fails
+
+set -e
+
 export AWS_DEFAULT_REGION=us-east-2
 
 if [[ $BRANCH_NAME == "main" ]]
 then
     cd api/
-    make build namespace=main
-    make push namespace=main
-    make deploy namespace=main
+    make build 
+    make push 
+    make deploy stage=main
     cd ..
     cd web/
-    make build namespace=main
-    make push namespace=main
-    make deploy namespace=main
+    make build 
+    make push 
+    make deploy stage=main
     cd ..
 elif [[ $BRANCH_NAME == "dev" ]]
 then
     cd api/
-    make build namespace=dev
-    make push namespace=dev
-    make deploy namespace=dev
+    make build 
+    make push 
+    make deploy stage=dev
     cd ..
     cd web/
-    make build namespace=dev
-    make push namespace=dev
-    make deploy namespace=dev
+    make build 
+    make push 
+    make deploy stage=dev
     cd ..
 elif [[ $BRANCH_NAME == "prod" ]]
 then
     cd api/
-    make build namespace=prod
-    make push namespace=prod
-    make deploy namespace=prod
+    make build 
+    make push 
+    make deploy stage=prod
     cd ..
     cd web/
-    make build namespace=prod
-    make push namespace=prod
-    make deploy namespace=prod
+    make build 
+    make push 
+    make deploy stage=prod
     cd ..
 fi    
