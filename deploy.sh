d3271881dbd8741aa07cca187668a1fd51f78aed
Alexkdevops <alexkad.devops@gmail.com>
2022-01-10 00:14:37 +0000

    Changes

--- deploy.sh
+++ deploy.sh
@@ -8,38 +8,22 @@ export AWS_DEFAULT_REGION=us-east-2
 
 if [[ $BRANCH_NAME == "main" ]]
 then
-    cd api/
-    make build stage=main
-    make push stage=main
-    make deploy stage=main
-    cd ..
-    cd web/
-    make build stage=main
-    make push stage=main
-    make deploy stage=main
-    cd ..
+    stage=main
 elif [[ $BRANCH_NAME == "dev" ]]
 then
-    cd api/
-    make build stage=dev
-    make push stage=dev
-    make deploy stage=dev
-    cd ..
-    cd web/
-    make build stage=dev
-    make push stage=dev
-    make deploy stage=dev
-    cd ..
+    stage=dev
 elif [[ $BRANCH_NAME == "prod" ]]
 then
-    cd api/
-    make build stage=prod
-    make push stage=prod
-    make deploy stage=prod
-    cd ..
-    cd web/
-    make build stage=prod
-    make push stage=prod
-    make deploy stage=prod
-    cd ..
+    stage=prod
 fi    
+
+cd api/
+make build stage=$(stage)
+make push stage=$(stage)
+make deploy stage=$(stage)
+cd ..
+cd web/
+make build stage=$(stage)
+make push stage=$(stage)
+make deploy stage=$(stage)
+cd ..
