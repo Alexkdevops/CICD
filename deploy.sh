1dfba4bba825f296beac33559606e7420057fb2b
AlexK <alexkadantsev@gmail.com>
2022-02-15 00:52:13 +0000

    new

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,13 @@
 #!/bin/bash
 
-# this will force script to exit if any command fails
-
 set -ex # good for bash-scripting trublesoots
-
 export AWS_DEFAULT_REGION=us-east-2
 
-if [[ $BRANCH_NAME == "main" ]]
-then
-    cd api/
-    make build stage=main
-    make push stage=main
-    make deploy stage=main
-    cd ..
-    cd web/
-    make build stage=main
-    make push stage=main
-    make deploy stage=main
-    cd ..
-elif [[ $BRANCH_NAME == "dev" ]]
-then
-    cd api/
-    make build stage=dev
-    make push stage=dev
-    make deploy stage=dev
-    cd ..
-    cd web/
-    make build stage=dev
-    make push stage=dev
-    make deploy stage=dev
-    cd ..
-elif [[ $BRANCH_NAME == "prod" ]]
-then
-    cd api/
-    make build stage=prod
-    make push stage=prod
-    make deploy stage=prod
-    cd ..
-    cd web/
-    make build stage=prod
-    make push stage=prod
-    make deploy stage=prod
-    cd ..
-fi    
+cd api/
+make build 
+make push 
+make deploy 
+cd ..
+
+
+ 
